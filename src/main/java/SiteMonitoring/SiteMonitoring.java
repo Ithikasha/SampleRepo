abc905909cbc79e18e07ae0bc477caf52605bfe7
Ithikash <56120004+Ithikasha@users.noreply.github.com>
2025-11-06 15:58:59 +0530

    Comment out SendingEmail usage in SiteMonitoring
    Comment out the SendingEmail import and instance.

--- src/main/java/SiteMonitoring/SiteMonitoring.java
+++ src/main/java/SiteMonitoring/SiteMonitoring.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 import Data_Functions.*;
-import Email.SendingEmail;
+//import Email.SendingEmail;
 import Regression.ContactUsForm;
 import Regression.Homepage;
 import Smoke_test.Data_smoke;
@@ -27,7 +27,7 @@ public class SiteMonitoring {
 	
 	public static SiteMonitoringConstantData SiteData = new SiteMonitoringConstantData();
 	
-	public static SendingEmail email = new SendingEmail();
+//	public static SendingEmail email = new SendingEmail();
     
     public static Data_smoke data_smoke = new Data_smoke();
     
