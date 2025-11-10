92a0061496de45caf785b0c40497324d71a0816d
Ithikash <56120004+Ithikasha@users.noreply.github.com>
2025-11-10 14:51:10 +0530

    Comment out SysoLogger import
    Comment out the import statement for SysoLogger.

--- src/main/java/Data_Functions/Functions.java
+++ src/main/java/Data_Functions/Functions.java
@@ -31,7 +31,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.itextpdf.text.log.SysoLogger;
+// import com.itextpdf.text.log.SysoLogger;
 
 import Regression.*;
 import SiteMonitoring.SiteMonitoringConstantData;
